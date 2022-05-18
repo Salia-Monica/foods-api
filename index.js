9d520bb8cdc3a5e2b1a3dee7c57364cd3d693446
Salia-Monica <saliamonica78@gmail.com>
2022-05-18 12:16:06 +0000

    added

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require("express")
-const morgan = require("morgan")
-require("dotenv").config()
-const connectDB =require("./config/connectDB")
-const foodRoute =require("./routes/foodRoute")
-const userRoute = require ("./routes/userRoute")
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const connectDB = require("./config/connectDB");
+const foodRoute = require("./routes/foodRoute");
+const userRoute = require("./routes/userRoute");
 
-const app = express()
+const app = express();
 
-connectDB()
+connectDB();
 
 //middleware
-app.use(express.json())
-app.use(morgan("dev"))
-app.use("/api/foods", foodRoute)
-app.use("/api/users", userRoute)
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/foods", foodRoute);
+app.use("/api/users", userRoute);
 
-const PORT =process.env.PORT||9000
+const PORT = process.env.PORT || 9000;
 //Home route
-app.get("/", (req, res)=>{
-    res.json("Welcome to my Menu API")
-})
+app.get("/", (req, res) => {
+  res.json("Welcome to my Menu API");
+});
 
-
-
-app.listen(PORT, ()=>{
-    console.log("Running")
-})
+app.listen(PORT, () => {
+  console.log("Running");
+});
